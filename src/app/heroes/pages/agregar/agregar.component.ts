eb11ab7adfa7804b375ab446682d0b8968eb2e70
Julian Gonzalez <felipetapsandes2@gmail.com>
2023-01-12 22:16:43 -0500

    Added CRUD logic

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators'
+
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ConfirmarComponent } from '../../components/confirmar/confirmar.component';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styleUrls: ['./agregar.component.css']
 })
-export class AgregarComponent {
+export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
+
+  constructor(private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private snackBar: MatSnackBar,
+              private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    if(!this.router.url.includes('editar')) {
+      return;
+    }
+
+    this.activatedRoute.params
+      .pipe(switchMap(({id}) => this.heroesService.getHeroePorId(id))
+      )
+      .subscribe((heroe) => this.heroe = heroe);
+  }
+
+  guardar(): void {
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    if (this.heroe.id) {
+      //Actualizar
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe(heroe => this.mostrarSnackBar('Registro Actualizado'));
+    } else {
+      //Crear
+      this.heroesService.agregarHeroe(this.heroe)
+      .subscribe(heroe => {   
+          this.router.navigate(['/heroes/editar', heroe.id]);
+          this.mostrarSnackBar('Registro Creado');
+        });
+    }
+  }
+
+  borrarHeroe(): void {
+
+    const dialog = this.dialog.open( ConfirmarComponent, {
+      width: '250px',
+      height: '160px',
+      data: this.heroe
+    });
+
+    dialog.afterClosed().subscribe(
+      (result) => {
+        if (result) {
+          this.heroesService.borrarHeroe(this.heroe.id!)
+            .subscribe( resp => {
+              this.router.navigate(['/heroes']);
+            });
+        }
+      }
+    )
+  }
+
+  mostrarSnackBar( mensaje: string): void {
+    this.snackBar.open(mensaje, 'ok!', {
+      duration: 2500
+    });
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -18,10 +18,22 @@ export class HeroesService {
   }
 
   getHeroePorId(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 
   getSugerencias(termino: string): Observable<Heroe[]> {
     return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`);
+  }
+
+  agregarHeroe( heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe);
+  }
+
+  actualizarHeroe( heroe: Heroe): Observable<Heroe> {
+    return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe);
+  }
+
+  borrarHeroe( id: string): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/heroes/${id}`);
   }
 }
